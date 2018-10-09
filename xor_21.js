f3d691b27b38a107dc8a35eccef34895454d7d9e
JohnCodd <john.codd@hotmail.com>
2018-10-09 12:04:59 +0100

    Added array functionality

--- xor_21.js
+++ xor_21.js
@@ -1,9 +1,19 @@
 /*! xor_21 v0.0.0 - MIT license */
 'use strict';
 
-var xor_21 = function (num)
+var xor_21 = function (input)
 {
-    return num ^ 21;
+    if (typeof input === "number") { return input ^ 21; }
+    if (typeof input === typeof [])
+    {
+        if (input.length === 0) { return []; }
+        let answer = [];
+        for (let value of input)
+        {
+            answer.push(value ^ 21);
+        }
+        return answer;
+    }
 }
 
 if (typeof module !== "undefined")
